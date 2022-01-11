91afd62e517cc218068f43e56774a9ca663434c3
Az1009 <qubakavkas@hotmail.de>
2022-01-11 15:13:14 +0100

    added Immages and slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '12.angular';
+export class AppComponent implements OnInit{
+
+  //array für die Bilder
+   images = ['1.jpeg', '2.jpeg', '3.jpeg'];
+   currentImage = 0;
+   showImage = true;
+
+   // um die datei auszuführen
+   ngOnInit(){
+     this.updateImage();
+
+   }
+
+   // funktion um bilder zu laden 
+updateImage(){
+// Bilder austauschen
+  setInterval(()=>{
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length;
+    this.showImage = false;
+
+    setTimeout(() =>{
+      this.showImage = true;
+    }, 10);
+
+  }, 8000);
+
+  
+}     
 }
+
+  
