d0a624c7870daa27b69552818b1ac9e235433262
Az1009 <qubakavkas@hotmail.de>
2022-01-12 11:42:32 +0100

    darkMode funktioniert nicht

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImgSlideContainerComponent } from './img-slide-container/img-slide-container.component';
+import{ImprintComponent} from './imprint/imprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ImgSlideContainerComponent},
+
+  // wenn Phat /imprint ist,
+  {path: 'imprint', component: ImprintComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
